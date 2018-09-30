968d6c9e72966304ac802b647fb5143677421a7b
Aaron Heidorn <aaron.heidorn@healthcaresource.com>
2018-09-30 08:45:13 -0400

    login

--- cli/app/controllers/application.js
+++ cli/app/controllers/application.js
@@ -1,11 +1,18 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  session: service(),
+  
   showMenu: false,
+  showLogin: false,
 
   actions: {
     toggleShowMenu: function(value = null) {
       this.set('showMenu', value != null ? value : !this.showMenu);
+    },
+    toggleShowLogin: function() {      
+      this.toggleProperty('showLogin');
     }
   }
 });
--- cli/app/services/session.js
+++ cli/app/services/session.js
@@ -1,5 +1,6 @@
 import Service from '@ember/service';
 
 export default Service.extend({
+  isAuthenticated: false,
   token: null
 });
